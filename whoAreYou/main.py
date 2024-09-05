9771ecf8acfded1cad5730a3727d3ffde5e042c4
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-05 09:30:05 -0600

    I wrote all the code, and the program runs as intended.

--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -2,4 +2,4 @@
 UserName = input("Why, hello there! What's your name? ")
 UserAge = input("\nOkay... How old are you? ")
 UserFavColor = input("\nI see! What is your favorite color? ")
-print("\nSo, you are " + UserName + ", you are " + UserAge + " years old, and your favorite color is " + UserFavColor + ", huh? Cool! Nice to meet you! XD")
+print("\nSo, you are " + UserName + ", you are " + UserAge + " years old, and your favorite color is " + UserFavColor + ", huh? Cool! Nice to meet you! B^)")
