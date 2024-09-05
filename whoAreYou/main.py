ce34be0e9e8864157894b7e38bf168a33380a4a5
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-05 09:29:14 -0600

    Awesome as always; I'm finished. 8 )

---
+++ whoAreYou/main.py
@@ -0,0 +1,5 @@
+# Evan McCabe SkillPractice: Who are you?
+UserName = input("Why, hello there! What's your name? ")
+UserAge = input("\nOkay... How old are you? ")
+UserFavColor = input("\nI see! What is your favorite color? ")
+print("\nSo, you are " + UserName + ", you are " + UserAge + " years old, and your favorite color is " + UserFavColor + ", huh? Cool! Nice to meet you! XD")
