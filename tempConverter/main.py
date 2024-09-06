d05e1a8ba27ae7763155bf4754702c597fa2e57b
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-06 09:47:06 -0600

    I have finished this program. It converts Celsius to Fahrenheit.

---
+++ tempConverter/main.py
@@ -0,0 +1,5 @@
+#Evan McCabe ProficiencyTest: Celsius to Fahrenheit
+tempCels = int(input("Please enter a temperature in Celsius: "))
+tempFahr = str((tempCels * 9/5)+32)
+tempCels = str(tempCels)
+print(tempCels + " degrees Celsius is " + tempFahr + " degrees Fahrenheit.")
