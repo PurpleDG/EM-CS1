36e28c54c76c4071e477270c10ee8890530ec4b3
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-09 09:40:16 -0600

    I have finished my code for a Madlib game, where the user can input words that are integrated into a story.

---
+++ madlib/main.py
@@ -0,0 +1,11 @@
+#Evan McCabe ProfiencyTest: Madlib
+print("Hello, welcome to Madlib! Please input the requested words to make a story.")
+adj1 = str(input("Please input an adjective - a feeling: "))
+nou1 = str(input("Please input a noun - a vehicle: "))
+nou2 = str(input("Please input a noun - somewhere you can order food: "))
+nou3 = str(input("Please input a noun - a food: "))
+nou4 = str(input("Please input a noun - a location: "))
+nou5 = str(input("Please input a noun - an exercise: "))
+adj2 = str(input("Please input an adjective: "))
+
+print("\nI was feeling " + adj1 + ", so I decided to drive my " + nou1 + " over to a " + nou2 + ".\nI ordered a plate of " + nou3 + ", but they gave me a " + nou1 + " instead for some reason.\nIt was delicious!\nAfterwards, I wanted to get some exercise in, so I went to " + nou4 + " and started doing " + nou5 + ".\nOverall, I had a " + adj2 + " day!\n\nDid you like the story? Was it " + adj1 + "?\n")
