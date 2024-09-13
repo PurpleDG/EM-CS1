3efc64060a4752cbfbf53570db6fcef7f1c83d82
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-13 09:18:31 -0600

    The error... dissapeared? I finished, and the program works! (:

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,6 +1,6 @@
 #Evan McCabe SkillPractice: Palandrom
 
-print("\nHello! I am the palindrome checker. I will tell you if a given word is a palindrome(If it is the same forwards and backwards).\n")
+print("\nHello! I am Palindrome Checker. I will tell you if a given word is a palindrome(If it is the same forwards and backwards).\n")
 input = input("Input the word you want checked: ")
 input = input.replace(" ","")
 
@@ -8,3 +8,5 @@ if input == input[::-1]:
     print("That is a palindrome.")
 else:
     print("That is not a palindrome.")
+
+#I used W3Schools to find out how to flip a string around backwards.
