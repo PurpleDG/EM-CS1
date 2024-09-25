fd92bfedacf516de020c350476f8340c247ac2e4
PurpleDG <evan.mccabe@ucas-edu.net>
2024-09-25 09:46:07 -0600

    I finally finished my anagram creator, which makes 5 anagrams from the user's name.

--- anagramCreator/main.py
+++ anagramCreator/main.py
@@ -1,4 +1,14 @@
 #Evan McCabe Raid: Anagram Creator
 
-userName = input("\nHello I am the Anagram Creator. I can generate 5 anagrams from your name(though they may not be the best).\nWhat is your name? ")
+import random
 
+userName = list(input("\nHello I am the Anagram Creator. I can generate 5 anagrams from your name(though they may not be the best).\nWhat is your name? ").lower())
+userNameCopy = userName
+
+print("\n")
+for i in range(5):
+    random.shuffle(userNameCopy)
+    print(''.join(userNameCopy))
+    userNameCopy = userName
+
+print("\nThere you go! (:\n")
