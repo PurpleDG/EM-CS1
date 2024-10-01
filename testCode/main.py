840f1f95480235d76f208dca776018c10468afe8
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-01 09:52:48 -0600

    working on pseudocode assignment

--- testCode/main.py
+++ testCode/main.py
@@ -1,1 +1,44 @@
 #This is where I test code outside of a project.
+
+import random
+def dayAtSchool(day):
+    if day is Monday or Friday:
+        wake up at 7:00
+    else:
+        wake up at 6:00
+    shower
+    breakfast = random(breakfast cereal, oatmeal, granola)
+    if day is Monday, Tuesday, Thursday, or Friday:
+        eat breakfast
+        check messages
+    else:
+        check messages
+    get ready
+    if day is Monday or Friday
+        walk to the bus Stop
+        scan card
+        get on bus 
+        get off bus 
+        walk to school 
+    else:
+        walk to the car 
+        get in the car 
+        get out of car 
+        go to seminary 
+        if day is Wednesday:
+            seminaryBreakfast = What was brought for breakfast?
+            eat seminaryBreakfast
+        leave seminary 
+        walk to bus stop 
+        get on bus 
+        scan card 
+        get off bus 
+        walk to school 
+    go to Computer Science
+    put phone in the phone garage
+    complete assignments
+    get phone out of the phone garage
+    talk to friends outside of math class
+    go to math class
+    
+
