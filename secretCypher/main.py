a177927c993808861633af4a9a057b89d9966d73
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-02 09:47:34 -0600

    I fixed an error where the program coded letters that had already been coded(Making the message IMPOSSIBLE to decode).

--- secretCypher/main.py
+++ secretCypher/main.py
@@ -10,57 +10,58 @@ coded = coded.lower()
 def code():
     global coded
     if "a" in coded:
-        coded = coded.replace("a", "i")
+        coded = coded.replace("a", "I")
     if "b" in coded:
-        coded = coded.replace("b", "j")
+        coded = coded.replace("b", "J")
     if "c" in coded:
-        coded = coded.replace("c", "k")
+        coded = coded.replace("c", "K")
     if "d" in coded:
-        coded = coded.replace("d", "l")
+        coded = coded.replace("d", "L")
     if "e" in coded:
-        coded = coded.replace("e", "m")
+        coded = coded.replace("e", "M")
     if "f" in coded:
-        coded = coded.replace("f", "n")
+        coded = coded.replace("f", "N")
     if "g" in coded:
-        coded = coded.replace("g", "o")
+        coded = coded.replace("g", "O")
     if "h" in coded:
-        coded = coded.replace("h", "p")
+        coded = coded.replace("h", "P")
     if "i" in coded:
-        coded = coded.replace("i", "q")
+        coded = coded.replace("i", "Q")
     if "j" in coded:
-        coded = coded.replace("j", "r")
+        coded = coded.replace("j", "R")
     if "k" in coded:
-        coded = coded.replace("k", "s")
+        coded = coded.replace("k", "S")
     if "l" in coded:
-        coded = coded.replace("l", "t")
+        coded = coded.replace("l", "T")
     if "m" in coded:
-        coded = coded.replace("m", "u")
+        coded = coded.replace("m", "U")
     if "n" in coded:
-        coded = coded.replace("n", "v")
+        coded = coded.replace("n", "V")
     if "o" in coded:
-        coded = coded.replace("o", "w")
+        coded = coded.replace("o", "W")
     if "p" in coded:
-        coded = coded.replace("p", "x")
+        coded = coded.replace("p", "X")
     if "q" in coded:
-        coded = coded.replace("q", "y")
+        coded = coded.replace("q", "Y")
     if "r" in coded:
-        coded = coded.replace("r", "z")
+        coded = coded.replace("r", "Z")
     if "s" in coded:
-        coded = coded.replace("s", "a")
+        coded = coded.replace("s", "A")
     if "t" in coded:
-        coded = coded.replace("t", "b")
+        coded = coded.replace("t", "B")
     if "u" in coded:
-        coded = coded.replace("u", "c")
+        coded = coded.replace("u", "C")
     if "v" in coded:
-        coded = coded.replace("v", "d")
+        coded = coded.replace("v", "D")
     if "w" in coded:
-        coded = coded.replace("w", "e")
+        coded = coded.replace("w", "E")
     if "x" in coded:
-        coded = coded.replace("x", "f")
+        coded = coded.replace("x", "F")
     if "y" in coded:
-        coded = coded.replace("y", "g")
+        coded = coded.replace("y", "G")
     if "z" in coded:
-        coded = coded.replace("z", "h")
+        coded = coded.replace("z", "H")
+    coded = coded.lower()
 
 code()
 print("\nYour message: " + uncoded + "\n\nCoded message: " + coded + "\n")
