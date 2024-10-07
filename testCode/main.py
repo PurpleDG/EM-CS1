8f6ba4e18b68291c7a6a31aa7687f335170c9ae0
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-07 09:53:37 -0600

    I continued working on my school day psuedocode.

--- testCode/main.py
+++ testCode/main.py
@@ -36,9 +36,81 @@ def dayAtSchool(day):
         walk to school 
     go to Computer Science
     put phone in the phone garage
+    sit down 
+    if morningGame == blooket
+        join blooket
+    elif morningGame == gimkit
+        join gimkit
+    win
     complete assignments
     get phone out of the phone garage
     talk to friends outside of math class
     go to math class
+    take calculator
+    put phone in phone garage
+    sit down 
+    hand sanitizer
+    if test == True
+        stretch
+        tell jokes
+        take test
+    else:
+        complete refresh/rewind
+        take notes
+        write down homework
+    get phone out of the phone garage
+    put calculator away
+    walk to english
+    put phone in phone garage
+    get notebook
+    complete english notebook starter
+    pass notebook forward
+    read
+    complete assignment
+    take phone out of the garage
+    go to lunch line 
+    get lunch
+    get utensils and sauces
+    get napkin
+    if hope squad == True
+        go to hope squad
+        sit down
+        hand sanitizer
+        eat lunch
+        do hope squad things
+    else:
+        sit down 
+        hand sanitizer
+        eat lunch
+        chat
+        rush to advisory
+        put phone in phone garage
+        do homework
+        take phone out of phone garage
+    go to geography
+    put phone in phone garage
+    sit down
+    do quiz
+    do timed map
+    take notes
+    do assignment
+    take phone out of phone garage
+    go to art 
+    sit down
+    if pockets not good:
+        put phone in backpack
+    do starter
+    draw 
+    chat 
+    go to biology
+    put phone in phone garage
+    take papers
+    if color coded notes == True
+        take colored pencils
+    sit down 
+    if lab day == True
+        go to lab room 
+        complete lab
+    else:
+        take notes
     
-
