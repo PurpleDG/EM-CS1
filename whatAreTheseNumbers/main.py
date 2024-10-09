619336156960ae50e34442fa5c31e12afe5f36de
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-09 09:51:41 -0600

    I have started coding a program that formats a given number in various ways.

---
+++ whatAreTheseNumbers/main.py
@@ -0,0 +1,8 @@
+#Evan McCave ProficiencyTest: What are these numbers?
+
+print("\nHello, I am the number formatter.")
+
+def formatNum():
+    num = input("What number would you like formatted? ")
+
+formatNum()
