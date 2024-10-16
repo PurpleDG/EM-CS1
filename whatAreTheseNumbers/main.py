927ec38cb2952f2734355500ad035c88096a6e69
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-16 09:48:59 -0600

    It's not working. :\

--- whatAreTheseNumbers/main.py
+++ whatAreTheseNumbers/main.py
@@ -3,11 +3,10 @@
 print("\nHello, I am the number formatter.")
 
 def formatNum():
-    txt0 = "{:,}"
+    txt0 = "{:,} "
     num = input("What number would you like formatted? ")
     print("\nHere it is, formatted:")
     print(txt0.format(num))
     
-
 
 formatNum()
