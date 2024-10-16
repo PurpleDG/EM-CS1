6b736a4e7bf6b8664fe8742ab3cba0504890a533
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-16 09:46:28 -0600

    I fixed my program so that it prints the multiples of a user inputted number 1-12, not the factors of it.

--- multiplicationTable/main.py
+++ multiplicationTable/main.py
@@ -1,7 +1,6 @@
 #Evan McCabe ProficiencyTest: Multiplication Table
 
 num = int(input("\nHello, user! I am the Multiplication Table bot. I can find the multiples of a number 1-12.\nWhat number would you like the multiples of? "))
-print("\n")
 
 a = 1
 
@@ -9,9 +8,8 @@ def findMultiples():
     global a
     global num
     for i in range(12):
-        if num % a == 0:
-            print(a, ' | ', num // a)
-        a +=1
+        print(num * a)
+        a += 1
     print("\nThere you go!\n")
 
 findMultiples()
