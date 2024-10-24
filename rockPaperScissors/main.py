bc2944dc1c040f983521750b35fc5dd48b7401b7
PurpleDG <evan.mccabe@ucas-edu.net>
2024-10-24 09:24:34 -0600

    I finished my rock, paper, scissors game program.

--- rockPaperScissors/main.py
+++ rockPaperScissors/main.py
@@ -2,7 +2,7 @@
 
 import random
 
-print("\nHello! Ready to play Rock, Paper, Scissors?\n")
+print("\nHello! Ready to play Rock, Paper, Scissors?")
 
 playing = True
 
@@ -12,13 +12,59 @@ def runGame():
     global playing
     global cpuOptions
     while playing == True:
-        move = input("What's your move? (r = rock, p = paper, s = scissors)\n")
+        move = input("\nWhat's your move? (r = rock, p = paper, s = scissors)\n")
         cpuMove = random.choice(cpuOptions)
         if move == cpuMove:
-            playAgain = input("\nTIE\n\nWant to play again? (y for yes, no for no)\n")
+            playAgain = input("\nI chose the same thing!\n\nTIE\n\nWant to play again? (y for yes, no for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "r" and cpuMove == "p":
+            playAgain = input("\nI choose paper!\n\nYOU LOSE\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "r" and cpuMove == "s":
+            playAgain = input("\nI choose scissors!\n\nYOU WIN\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "p" and cpuMove == "r":
+            playAgain = input("\nI choose rock!\n\nYOU WIN\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "p" and cpuMove == "s":
+            playAgain = input("\nI choose scissors!\n\nYOU LOSE\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "s" and cpuMove == "p":
+            playAgain = input("\nI choose paper!\n\nYOU WIN\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
+        elif move == "s" and cpuMove == "r":
+            playAgain = input("\nI choose rock!\n\nYOU LOSE\n\nWant to play again? (y for yes, n for no)\n")
+            if playAgain == "y":
+                runGame()
+            else:
+                print("\nThanks for playing!\n")
+                break
         else:
-            print("\nInvalid input. Please try again.\n")
+            print("\nInvalid input. Please try again.")
             runGame()        
-
 
 runGame()
