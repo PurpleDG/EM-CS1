8790111d0d0ac2b9bedaedf0ed7da779333df4ab
PurpleDG <evan.mccabe@ucas-edu.net>
2024-11-07 09:35:25 -0700

    I made a simple program that asks the user for their name and greets them.

---
+++ commentChallenge/main.py
@@ -0,0 +1,8 @@
+#Evan McCabe Python Comments Challenge
+
+#This program asks the user what their name is before greeting them.
+
+#This is the user name variable, that asks the user for their name:
+userName = input("\nWhat is your name?\n")
+#This line of code prints a greeting to the user:
+print("\nHello, " + userName + "!\n")
