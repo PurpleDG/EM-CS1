c7800963fd8d48f8d4a625bcdda54b9bfe3a99b3
PurpleDG <evan.mccabe@ucas-edu.net>
2024-11-08 09:52:17 -0700

    I made a program that prints each item in a list, specifically, my classes.

---
+++ listOfClasses/main.py
@@ -0,0 +1,10 @@
+#Evan McCabe SkillPractice: List of Classes
+
+myClasses = ["Computer Science", "Math", "English", "Advisory", "Geography", "Art", "Biology"]
+
+print("\n")
+
+for i in myClasses:
+    print(i)
+
+print("\n")
