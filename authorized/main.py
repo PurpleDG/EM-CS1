7f47a253677dff5518a83ff41ba3b7b3181c6c6b
PurpleDG <evan.mccabe@ucas-edu.net>
2024-11-25 09:40:09 -0700

    I added myself. >:) (and some more friends)

--- authorized/main.py
+++ authorized/main.py
@@ -5,11 +5,14 @@ print("\nServer booting...")
 time.sleep(2)
 print("\nSign in to\n-ChimknNuggs-\n")
 user = input("Username: ")
+king = ["Evan"]
 admins = ["Vincent", "Pedro", "Dean"]
-users = ["Sam", "Xavier", "Alec","Addison", "Eve", "Cadence", "Josephine", "Red"]
+users = ["Sam", "Xavier", "Alec","Addison", "Eve", "Cadence", "Josephine", "Red", "Ben", "Will", "Matthew"]
 if user in admins:
     print("\n- AUTHORIZED -\n\nWelcome, " + user + "(admin)!\n")
 elif user in users:
     print("\n- AUTHORIZED -\n\nWelcome, " + user + "(user)!\n")
+elif user in king:
+    print("\n- AUTHORIZED -\n\nWelcome, your majesty.\n")
 else:
     print("\n- UNAUTHORIZED -\n")
